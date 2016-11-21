d1b3d2cca43ce8d757909837ce61ae4dff354b46
akifmt <akifmt@gmail.com>
2016-11-22 02:39:43 +0300

    added new exps

---
+++ 1-Beginner/22. Data Types/Assets/DatatypeScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DatatypeScript : MonoBehaviour {
+
+	void Start()
+    {
+        //Valus type variable
+        Vector3 pos = transform.position;
+        pos = new Vector3(0,2,0);
+
+        //Reference type vatiable
+        Transform tran = transform;
+        tran.position = new Vector3(0,2,0);
+    }
+}
---
+++ 1-Beginner/24. Instantiate/Assets/UsingInstantiate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UsingInstantiate : MonoBehaviour {
+
+    public Rigidbody rocketPrefab;
+    public Transform barrelEnd;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody rocketInstantiate;
+            rocketInstantiate = Instantiate(rocketPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+            rocketInstantiate.AddForce(barrelEnd.forward * 5000);
+        }
+    }
+}
