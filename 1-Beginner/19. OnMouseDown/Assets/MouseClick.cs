cee9d7b9dab36da5078841b782e1ee1917bc886b
akifmt <akifmt@gmail.com>
2016-11-21 17:49:03 +0300

    added new ex

---
+++ 1-Beginner/19. OnMouseDown/Assets/MouseClick.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClick : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+
+    void OnMouseDown()
+    {
+        rigidbody.AddForce(transform.forward * 500f);
+        rigidbody.useGravity = true;
+    }
+}
